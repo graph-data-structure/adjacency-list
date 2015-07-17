8e70f022c0be2a646682d7c50bc946ecc2af4a93
aureooms <aurelien.ooms@gmail.com>
2015-07-17 23:56:33 +0000

    tests drafts

--- js/dist/adjacency-list.js
+++ js/dist/adjacency-list.js
@@ -1,22 +1,165 @@
-( function ( ) {
+"use strict";
 
-'use strict' ;
+(function () {
 
-var definition = function ( exports , undefined ) {
+	"use strict";
 
+	var definition = function definition(exports, undefined) {
 
-/* js/src/dummy.js */
+		/* js/src/Edge.js */
 
-return exports ;
-} ;
-if ( typeof exports === "object" ) {
-	definition( exports ) ;
-}
-else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-adjacency-list" , [ ] , function ( ) { return definition( { } ) ; } ) ;
-}
-else if ( typeof window === "object" && typeof window.document === "object" ) {
-	definition( window["adjacencylist"] = { } ) ;
-}
-else console.error( "unable to detect type of module to define for aureooms-js-adjacency-list") ;
-} )( ) ;
+		var Edge = function Edge(u, v) {
+
+			this.u = u;
+			this.v = v;
+			this.iterator = null;
+			this.uiterator = null;
+			this.viterator = null;
+		};
+
+		exports.Edge = Edge;
+
+		/* js/src/List.js */
+
+		var List = function List(DLL) {
+
+			var Graph = function Graph() {
+
+				this.V = new DLL();
+				this.E = new DLL();
+			};
+
+			Graph.prototype.vadd = function () {
+
+				var v = new Vertex(new DLL());
+
+				v.iterator = this.V.push(v);
+
+				return v;
+			};
+
+			Graph.prototype.vdel = function (v) {
+
+				// remove all incident edges
+				var _iteratorNormalCompletion = true;
+				var _didIteratorError = false;
+				var _iteratorError = undefined;
+
+				try {
+					for (var _iterator = this.eitr(v)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+						var e = _step.value;
+						this.edel(e);
+					}
+				} catch (err) {
+					_didIteratorError = true;
+					_iteratorError = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion && _iterator["return"]) {
+							_iterator["return"]();
+						}
+					} finally {
+						if (_didIteratorError) {
+							throw _iteratorError;
+						}
+					}
+				}
+
+				// remove vertex
+				this.E.erase(v.iterator);
+			};
+
+			Graph.prototype.eadd = function (u, v) {
+
+				var e = new Edge(u, v);
+
+				// add to edge list
+				e.iterator = this.E.push(e);
+
+				// add to edge list of u
+				e.uiterator = u.E.push(e);
+
+				// add to edge list of v
+				e.viterator = v.E.push(e);
+
+				return e;
+			};
+
+			Graph.prototype.edel = function (e) {
+
+				// remove from edge list
+				this.E.erase(e.iterator);
+
+				// remove from edge list of u
+				e.u.E.erase(e.uiterator);
+
+				// remove from edge list of v
+				e.v.E.erase(e.viterator);
+			};
+
+			Graph.prototype.vitr = regeneratorRuntime.mark(function callee$3$0() {
+				return regeneratorRuntime.wrap(function callee$3$0$(context$4$0) {
+					while (1) switch (context$4$0.prev = context$4$0.next) {
+						case 0:
+							return context$4$0.delegateYield(this.V, "t0", 1);
+
+						case 1:
+						case "end":
+							return context$4$0.stop();
+					}
+				}, callee$3$0, this);
+			});
+
+			Graph.prototype.eitr = regeneratorRuntime.mark(function callee$3$0(v) {
+				return regeneratorRuntime.wrap(function callee$3$0$(context$4$0) {
+					while (1) switch (context$4$0.prev = context$4$0.next) {
+						case 0:
+							return context$4$0.delegateYield(v.E, "t0", 1);
+
+						case 1:
+						case "end":
+							return context$4$0.stop();
+					}
+				}, callee$3$0, this);
+			});
+
+			Graph.prototype.aeitr = regeneratorRuntime.mark(function callee$3$0() {
+				return regeneratorRuntime.wrap(function callee$3$0$(context$4$0) {
+					while (1) switch (context$4$0.prev = context$4$0.next) {
+						case 0:
+							return context$4$0.delegateYield(this.E, "t0", 1);
+
+						case 1:
+						case "end":
+							return context$4$0.stop();
+					}
+				}, callee$3$0, this);
+			});
+
+			return Graph;
+		};
+
+		exports.List = List;
+
+		/* js/src/Vertex.js */
+
+		var Vertex = function Vertex(edges) {
+
+			this.iterator = null;
+			this.E = edges;
+		};
+
+		exports.Vertex = Vertex;
+
+		return exports;
+	};
+	if (typeof exports === "object") {
+		definition(exports);
+	} else if (typeof define === "function" && define.amd) {
+		define("aureooms-js-adjacency-list", [], function () {
+			return definition({});
+		});
+	} else if (typeof window === "object" && typeof window.document === "object") {
+		definition(window["adjacencylist"] = {});
+	} else console.error("unable to detect type of module to define for aureooms-js-adjacency-list");
+})();
--- test/js/src/List.js
+++ test/js/src/List.js
@@ -1,6 +1,11 @@
 
-var dll = require( "aureooms-js-dll" ) ;
+
 var itertools = require( "aureooms-js-itertools" ) ;
+
+var  ex = itertools.exhaust ;
+var map = itertools.map ;
+
+var dll = require( "aureooms-js-dll" ) ;
 
 test( 'List' , function ( ) {
 
@@ -8,203 +13,152 @@ test( 'List' , function ( ) {
 
 	var g = new Graph( ) ;
 
-	var v = [];
-	var e = [];
+	var v = [ ] ;
+	var e = [ ] ;
 
-	var n = 11;
+	var n = 11 ;
 
-	for(var i = 0; i < n; ++i){
-		v[i] = g.vadd();
-	}
+	for( var i = 0 ; i < n ; ++i ) v[i] = g.vadd( ) ;
 
-	e[1] = [];
-	e[1][0] = g.eadd(v[1], v[9], 18);
-	e[0] = [];
-	e[0][0] = g.eadd(v[0], v[10], 7);
-	e[0][1] = g.eadd(v[0], v[5], 2);
-	e[0][2] = g.eadd(v[0], v[3], 5);
-	e[0][3] = g.eadd(v[0], v[1], 456);
-	e[0][4] = g.eadd(v[0], v[7], 5);
-	e[0][5] = g.eadd(v[0], v[0], 5);
+	e[1] = [ ] ;
+	e[1][0] = g.eadd(v[1], v[9]);
+	e[0] = [ ] ;
+	e[0][0] = g.eadd(v[0], v[10]);
+	e[0][1] = g.eadd(v[0], v[5]);
+	e[0][2] = g.eadd(v[0], v[3]);
+	e[0][3] = g.eadd(v[0], v[1]);
+	e[0][4] = g.eadd(v[0], v[0]);
 	e[1][1] = e[0][3];
-	e[4] = [];
-	e[4][0] = g.eadd(v[4], v[7], 5);
+	e[4] = [ ] ;
+	e[4][0] = g.eadd(v[4], v[7]);
 
-	var k = 0;
-	g.vitr(function(j){
-		deepEqual(j, v[k], 'vitr ' + k);
-		++k;
-	});
-	deepEqual(k, v.length, 'check vertex count before del');
+	var r = [ 0 , 1 , 4 ] ;
 
-	var r = [0, 1, 4];
+	// tests
 
-	for(var l = 0; l < r.length; ++l){
-		var m = r[l];
-		k = e[m].length;
-		ok(k > 0, m + ' : check edge count before itr');
-		g.eitr(v[m], function(x){
-			--k;
-			deepEqual(x, e[m][k], 'eitr ' + m + ' ' + k);
-		});
-		deepEqual(k, 0, m + ' : check edge count before del');
-	};
+	var k , set , alledges ;
 
+	k = 0 ;
+	set = new WeakSet( v ) ;
+
+	ex( map( function ( j ) {
+		ok( set.has( j ) , 'vitr ' + k ) ;
+		set.delete( j ) ;
+		++k ;
+	} , g.vitr( ) ) ) ;
+
+	k = 0 ;
+	alledges = e[0].concat( [ e[1][0] ] ).concat( e[4] ) ;
+	set = new WeakSet( alledges ) ;
+
+	ex( map( function ( j ) {
+		ok( set.has( j ) , 'aeitr ' + k ) ;
+		set.delete( j ) ;
+		++k ;
+	} , g.aeitr( ) ) ) ;
+
+	deepEqual( k , alledges.length , 'check edges count before del' ) ;
+
+
+
+	r.forEach( function ( m ) {
+
+		var k = e[m].length ;
+
+		var set = new WeakSet( e[m] ) ;
+
+		ex( map( function ( x ) {
+			--k ;
+			ok( set.has( x ) , 'eitr ' + m + ' ' + k ) ;
+			set.delete( x ) ;
+		} , g.eitr( v[m] ) ) ) ;
+
+	} ) ;
+
+
+	// delete edges 1 0 , 0 2 , 4 0
 	g.edel(e[1].splice(0, 1)[0]);
 
 	g.edel(e[0].splice(2, 1)[0]);
 
 	g.edel(e[4].splice(0, 1)[0]);
 
+	k = 0 ;
+	alledges = e[0].concat( e[4] ) ;
+	set = new WeakSet( alledges ) ;
 
-	for(var l = 0; l < r.length; ++l){
-		var m = r[l];
-		k = e[m].length;
-		g.eitr(v[m], function(x){
-			--k;
-			deepEqual(x, e[m][k], 'eitr ' + m + ' ' + k);
-		});
-		deepEqual(k, 0, m + ' : check edge count after del');
-	};
+	ex( map( function ( j ) {
+		ok( set.has( j ) , 'aeitr ' + k ) ;
+		set.delete( j ) ;
+		++k ;
+	} , g.aeitr( ) ) ) ;
+
+	deepEqual( k , alledges.length , 'check edges count after del' ) ;
+
+	k = 0 ;
+	set = new WeakSet( map ( function ( e ) {
+		return e[0] === v[0] ? e[1] : e[0] ;
+	} , e[0] ) ) ;
+
+	ex( map( function ( j ) {
+		ok( set.has( j ) , 'nitr ' + k ) ;
+		set.delete( j ) ;
+		++k ;
+	} , g.nitr( v[0] ) ) ) ;
+
+	deepEqual( k , e[0].length , 'check neighbour count after del' ) ;
+
+	r.forEach( function ( m ) {
+
+		var k = e[m].length ;
+
+		var set = new WeakSet( e[m] ) ;
+
+		ex( map( function ( x ) {
+			--k ;
+			ok( set.has( x ) , 'eitr ' + m + ' ' + k ) ;
+			set.delete( x ) ;
+		} , g.eitr( v[m] ) ) ) ;
+
+	} ) ;
+
+	// delete vertex 3
 
 	g.vdel(v.splice(3, 1)[0]);
 
-	k = 0;
-	g.vitr(function(j){
-		deepEqual(j, v[k], 'vitr ' + k);
-		++k;
-	});
+	k = 0 ;
+	set = new WeakSet( v ) ;
 
-	e[1].splice(0, 1);
+	ex( map( function ( j ) {
+		ok( set.has( j ) , 'vitr ' + k ) ;
+		set.delete( j ) ;
+		++k ;
+	} , g.vitr( ) ) ) ;
 
-	for(var l = 0; l < r.length; ++l){
-		var m = r[l];
-		while(e[m].length){
-			g.edel(e[m].splice(0, 1)[0]);
-		}
-	};
+	deepEqual( k , v.length , 'check vertex count after del' ) ;
 
-	g.vitr(function(i){
-		g.eitr(i, function(e){
+	e[0].splice(2, 1);
+
+	// delete remaining edges
+	r.forEach( function ( m ) {
+		while ( e[m].length ) g.edel( e[m].splice( 0 , 1 )[0] ) ;
+	} ) ;
+
+	ex( map( function ( e ) {
+		ok( false , 'aeitr never go here') ;
+	} , g.aeitr( ) ) ) ;
+
+	ex( map( function ( i ) {
+		ex( map( function ( e ) {
 			ok(false, 'eitr never go here');
-		});
-	});
+		} , g.eitr( i ) ) ) ;
+	} , g.vitr( ) ) ) ;
 
-	while(v.length){
-		g.vdel(v.splice(0, 1)[0]);
-	}
+	// delete remaining vertices
+	while ( v.length ) g.vdel(v.splice(0, 1)[0]) ;
 
-	g.vitr(function(i){
-		ok(false, 'vitr never go here');
-	});
-
-});
-
-
-
-
-
-
-
-
-test('sparse 2', function(assert){
-	var Graph = gn.sparse_graph_t();
-
-	var g = new Graph();
-
-	var v = [];
-	var e = [];
-
-	var n = 11;
-
-	for(var i = 0; i < n; ++i){
-		v[i] = g.vadd();
-	}
-
-	e[1] = [];
-	e[1][0] = g.eadd(v[1], v[9], 18);
-	e[0] = [];
-	e[0][0] = g.eadd(v[0], v[10], 7);
-	e[0][1] = g.eadd(v[0], v[5], 2);
-	e[0][2] = g.eadd(v[0], v[3], 5);
-	e[0][3] = g.eadd(v[0], v[1], 456);
-	e[0][4] = g.eadd(v[0], v[7], 5);
-	e[0][5] = g.eadd(v[0], v[0], 5);
-	e[1][1] = e[0][3];
-	e[4] = [];
-	e[4][0] = g.eadd(v[4], v[7], 5);
-
-	var k = 0;
-	g.vitr(function(j){
-		deepEqual(j, v[k], 'vitr ' + k);
-		++k;
-	});
-	deepEqual(k, v.length, 'check vertex count before del');
-
-	var r = [0, 1, 4];
-
-	for(var l = 0; l < r.length; ++l){
-		var m = r[l];
-		k = e[m].length;
-		ok(k > 0, m + ' : check edge count before itr');
-		g.eitr(v[m], function(x){
-			--k;
-			deepEqual(x, e[m][k], 'eitr ' + m + ' ' + k);
-		});
-		deepEqual(k, 0, m + ' : check edge count before del');
-	};
-
-	g.edel(e[1].splice(0, 1)[0]);
-
-	g.edel(e[0].splice(2, 1)[0]);
-
-	g.edel(e[4].splice(0, 1)[0]);
-
-
-	for(var l = 0; l < r.length; ++l){
-		var m = r[l];
-		k = e[m].length;
-		g.eitr(v[m], function(x){
-			--k;
-			deepEqual(x, e[m][k], 'eitr ' + m + ' ' + k);
-		});
-		deepEqual(k, 0, m + ' : check edge count after del');
-	};
-
-	g.vdel(v.splice(3, 1)[0]);
-
-	k = 0;
-	g.vitr(function(j){
-		deepEqual(j, v[k], 'vitr ' + k);
-		++k;
-	});
-
-
-	k = -1;
-	g.vitr(function(j){deepEqual(0, ++k, 'vitr stop'); return true;});
-
-
-	k = -1;
-	g.eitr(v[0], function(x){
-		deepEqual(0, ++k, 'eitr stop');
-		return true;
-	});
-
-	e[1].splice(0, 1);
-
-	for(k = 0; k < 10; ++k){
-		g.eadd(v[0], v[1], 456);
-		g.eadd(v[0], v[7], 2);
-		g.eadd(v[0], v[0], 1);
-	}
-
-	while(v.length){
-		g.vdel(v.splice(0, 1)[0]);
-	}
-
-	g.vitr(function(i){
-		ok(false, 'vitr never go here');
-	});
+	ex( map( function ( i ) {
+		ok( false , 'vitr never go here') ;
+	} , g.vitr( ) ) ) ;
 
 });
-
