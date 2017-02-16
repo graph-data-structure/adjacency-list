07f753ac9d4493a5829261c1b9b0f336de30371f
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:46 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/adjacency-list.js
+++ js/dist/adjacency-list.js
@@ -1428,10 +1428,10 @@ var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = 
 	if (typeof exports === "object") {
 		definition(exports);
 	} else if (typeof define === "function" && define.amd) {
-		define("aureooms-js-adjacency-list", [], function () {
+		define("@aureooms/js-adjacency-list", [], function () {
 			return definition({});
 		});
 	} else if (typeof window === "object" && typeof window.document === "object") {
 		definition(window["adjacencylist"] = {});
-	} else console.error("unable to detect type of module to define for aureooms-js-adjacency-list");
+	} else console.error("unable to detect type of module to define for @aureooms/js-adjacency-list");
 })();
--- test/js/src/spec.js
+++ test/js/src/spec.js
@@ -1,7 +1,7 @@
 
-var dll = require( "aureooms-js-dll" ) ;
+var dll = require( "@aureooms/js-dll" ) ;
 
-var spec = require( "aureooms-js-graph-spec" ) ;
+var spec = require( "@aureooms/js-graph-spec" ) ;
 
 spec.MultiGraph( "MultiGraph" , adjacencylist.MultiGraph( dll.DoublyLinkedList ) ) ;
 spec.MultiDiGraph( "MultiDiGraph" , adjacencylist.MultiDiGraph( dll.DoublyLinkedList , Map ) ) ;
