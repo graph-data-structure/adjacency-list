e9f2cfc8c786a0820ea3c69ced3747328c5e0136
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-11-14 21:47:45 +0100

    :wrench: config(test): Fix.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -10,7 +10,7 @@ const domReady = function (callback) {
 domReady(() => {
 	const projectname = document.createElement('a');
 	projectname.classList.add('project-name');
-	projectname.text = 'graph-data-structure/adjacency-list';
+	projectname.text = '@graph-data-structure/adjacency-list';
 	projectname.href = './index.html';
 
 	const header = document.querySelector('header');
